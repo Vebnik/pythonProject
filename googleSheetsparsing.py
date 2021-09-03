0fb66dc24a68b75008e7663b0f336b6e82b97474
NikIrk <40178104+Vebnik@users.noreply.github.com>
2021-09-03 12:43:05 +0300

    Update googleSheetsparsing.py

--- googleSheetsparsing.py
+++ googleSheetsparsing.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 # Ссылка на удалённый ресурс с таблицей.
-response = requests.get()
+response = requests.get('Вот тут ссылка на таблицу в сети/удалённом сервера с постоянным обновлением')
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -12,6 +12,7 @@ for time in time_table_event:
     #if(time == time.find_all('td', {'class':'s8'})):
     #    continue
     time = time.find_all('td', {'class':'s31'})
+    # Пока вывод на стороне консоли. Потом ответ будет улетать в БД под MySQL.
     print(time)
 
 #print(time_table_event)
